430d4da8cb82cf46e646e6e34370086459a4c834
Mauro <mauro.labattaglia@snappler.com>
2015-03-05 14:19:33 -0300

    subida inicial

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,27 @@
+module ApplicationHelper
+
+  def flash_message(klass, message)
+    klass = 'success' if klass == 'notice' || klass == 'alert'
+    flash.discard(klass)
+    content_tag :article, class: "alert alert-#{klass}" do
+      concat close_button(:alert)
+      concat message
+    end
+  end
+
+  def flash_messages
+    flash.map do |type, message|
+      flash_message(type, message)
+    end.join.html_safe
+  end
+
+  def close_button(target = :modal)
+    link_to '&times;'.html_safe, '', class: :close, data: {dismiss: target}
+  end
+
+  def icon(name, html_options={})
+    html_options[:class] = ['fa', "fa-#{name}", html_options[:class]].compact
+    content_tag(:i, nil, html_options)
+  end
+
+end
